dbd512f516ba8bf1c91c93983c1215a632c6cb00
Jonathan <jseik3n87@gmail.com>
2018-11-06 20:36:37 -0600

    Initial commit

---
+++ BST.java
@@ -0,0 +1,43 @@
+// The BST class that represents binary search trees
+// Your name here
+
+import java.util.*;
+
+public class BST 
+{
+    // instance variables
+    private BSTNode m_root;
+    private int m_size;
+    
+    // constructor
+    public BST()
+    {
+        m_root = null;
+        m_size = 0;
+    }
+    
+    // add a value into the tree
+    public void add(int v)
+    {
+        // TODO: implement this method using a non-recursive solution
+    }
+    
+    // print the tree content using in-order traveral
+    public void inOrder()
+    {   
+        // TODO: implement this method using a non-recursive solution
+    }
+    
+    // get the size of the tree
+    public int size()
+    {
+        return m_size;
+    }
+    
+    // empty the tree
+    public void clear()
+    {
+        m_root = null;
+        m_size = 0;
+    }
+}
