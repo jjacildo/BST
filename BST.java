69533a93ef0b2e8083892f9073eae676cd73e907
Jonathan <jseik3n87@gmail.com>
2018-11-09 04:32:15 -0600

    added inOrder( ) method

--- BST.java
+++ BST.java
@@ -58,8 +58,28 @@ public class BST
     
     // print the tree content using in-order traveral
     public void inOrder()
-    {   
+    {    
         // TODO: implement this method using a non-recursive solution
+        //check if tree is empty
+        if(m_root == null)
+            System.out.println("The tree is empty");
+        else{
+            Stack<BSTNode> mystack = new Stack<BSTNode>(); //create a stack to preserve order and enable to traverse the tree from left to root to right
+            
+            BSTNode crawler = m_root; //create a node that will traverse the tree, initialize it to the root of the tree
+            
+            while(!mystack.empty() || crawler != null){
+               if(crawler != null){
+                  mystack.push(crawler);//push current node into the stack
+                  crawler = crawler.getLeft();
+               }
+               else{
+                  BSTNode newNode = mystack.pop(); //create a new node for the last node pushed into the stack 
+                  System.out.print(newNode.getInfo() + " ");
+                  crawler = newNode.getRight();
+               }            
+            }              
+        }
     }
     
     // get the size of the tree
