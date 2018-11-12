75f6b80d44417aa75c9dddca8d9438464de3c460
Jonathan <jseik3n87@gmail.com>
2018-11-12 15:15:37 -0600

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,5 +1,5 @@
 // The BST class that represents binary search trees
-// Your name here
+// Jonathan Jacildo
 
 import java.util.*;
 
@@ -27,32 +27,30 @@ public class BST
             m_size++;
         }
         else{
-            BSTNode parent = m_root;
-            BSTNode child = m_root;
+            BSTNode nNode = m_root;
+            BSTNode crawler = m_root;
             //loop that will traverse through the tree
-            while (child != null){
-               parent = child;
+            while (crawler != null){
+               nNode = crawler;
                
-               if (v < child.getInfo())
-                  child = child.getLeft();
-               else if (v > child.getInfo())
-                  child = child.getRight();
+               if (v < crawler.getInfo())
+                  crawler = crawler.getLeft();
+               else if (v > crawler.getInfo())
+                  crawler = crawler.getRight();
                else
                   break;
             }
             //adds value into the BST
-            if (v < parent.getInfo()){
-               parent.setLeft(newNode);
+            if (v < nNode.getInfo()){
+               nNode.setLeft(newNode);
                m_size++;
             }
-            else if (v > parent.getInfo()){
-               parent.setRight(newNode);
+            else if (v > nNode.getInfo()){
+               nNode.setRight(newNode);
                m_size++;
             }
             else
-               return;
-            
-               
+               return;               
         }
     }
     
@@ -64,19 +62,19 @@ public class BST
         if(m_root == null)
             System.out.println("The tree is empty");
         else{
-            Stack<BSTNode> mystack = new Stack<BSTNode>(); //create a stack to preserve order and enable to traverse the tree from left to root to right
+            Stack<BSTNode> mystack = new Stack<BSTNode>(); //create a stack to preserve order and enabling to manipulate the tree from left to root to right
             
-            BSTNode crawler = m_root; //create a node that will traverse the tree, initialize it to the root of the tree
+            BSTNode crawler = m_root; //create a crawler node that will traverse the tree, initialize it to the root of the tree
             
             while(!mystack.empty() || crawler != null){
                if(crawler != null){
-                  mystack.push(crawler);//push current node into the stack
-                  crawler = crawler.getLeft();
+                  mystack.push(crawler);//push crawler node into the stack
+                  crawler = crawler.getLeft();//crawler moves to the left of the tree
                }
                else{
                   BSTNode newNode = mystack.pop(); //create a new node for the last node pushed into the stack 
-                  System.out.print(newNode.getInfo() + " ");
-                  crawler = newNode.getRight();
+                  System.out.print(newNode.getInfo() + " ");//prints out the value of the node that was popped from the top of the stack
+                  crawler = newNode.getRight();//moves to the right of the tree               
                }            
             }              
         }
